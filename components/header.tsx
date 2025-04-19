253b37ba531f7e1b3946f1548c65cc3d1ca067f6
= <ibraimovnursutan23@gmail.com>
2025-04-19 16:31:03 +0600

    updated

--- components/header.tsx
+++ components/header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
   useEffect(() => {
     document.body.style.overflow = isMenuOpen ? "hidden" : "auto"
     return () => {
-      document.body.style.overflow = "auto"
+      document.body.style.overflow = "hidden"
     }
   }, [isMenuOpen])
 
